5f021711337b9ecc2945ec144f645e4cf15e9ad6
imvickykumar999 <imvickykumar999@gmail.com>
2023-09-14 21:43:31 +0530

    Amplitude Graph

---
+++ Volume_Graph.py
@@ -0,0 +1,37 @@
+
+# https://colab.research.google.com/drive/1-Ilo-MFRbiCxiQEgAIcaU8TjvpHmMx-J#scrollTo=c0Mekc6Jw3ir
+
+import librosa, winsound
+import matplotlib.pylab as plt
+
+def save_plot(filename):
+    y, sr = librosa.load(filename)        
+    plt.xlabel('time')
+    plt.ylabel('amplitude')
+    plt.plot(y)
+    plt.savefig('test.png')
+    return y, sr
+
+y, sr = save_plot('test.wav')
+# https://youtu.be/S0nOYs0PRak
+
+scale = 5000
+with open('test.txt', 'w') as f:
+    for i, j in enumerate(y[::sr]):
+        try:
+            duration = 1000 # 1 sec
+            frequency = scale + int(scale*j)
+
+            winsound.Beep(frequency, duration)
+        except:
+            pass
+
+        print(i, scale*(1 + j))
+        f.write(f'{j}, ')
+
+print(len(y), sr, len(y)/sr, 'seconds')
+# 80000 22050 3.6281179138321997 seconds
+# ValueError: frequency must be in 37 thru 32767
+
+# The variable sr contains the sampling rate of y , 
+# that is, the number of samples per second of audio.
